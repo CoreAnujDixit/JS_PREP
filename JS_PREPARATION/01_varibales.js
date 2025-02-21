b7e39e477e6fd6a1c4de87bd5c190601f50f4eb1
CoreAnujDixit <=>
2025-02-21 19:09:19 +0530

    hELLO

---
+++ JS_PREPARATION/01_varibales.js
@@ -0,0 +1,15 @@
+const accountId = 2005;
+let accountEmail = "anujdixit201@gmail.com"
+var accountPassword = "anuj@2005"
+accountCity = "New Delhi"
+console.table(["Anuj", accountEmail, accountPassword, accountCity])
+
+//Datatype Conversion
+let new_value = String(accountId)
+console.log(typeof(new_value));
+
+//conversion
+
+let isLoggedIn = -2;
+let isReallyIn = console.log(Boolean(isLoggedIn));
+ 
