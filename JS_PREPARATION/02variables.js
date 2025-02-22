20198a42ab30b6f3fa78e90bc6d2a7c0ba4eebbf
CoreAnujDixit <=>
2025-02-22 18:46:17 +0530

    object

--- JS_PREPARATION/02variables.js
+++ JS_PREPARATION/02variables.js
@@ -1,3 +1,16 @@
 //Lecture 07 //Why string to number conversion is confusing | chai aur #javascript
 
 //postfix & Prefix done;
+
+
+//Lecture 08 - Comparison of datatypes in javascript | chai aur #javascript
+
+
+console.log(2 == 1);
+
+//object
+
+let myObj = {
+    name : "Anuj",
+    rollno : 12,
+}
