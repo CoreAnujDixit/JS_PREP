514d9b385ed665b7c3f8e9fed1245a04e61c7db3
CoreAnujDixit <=>
2025-03-20 18:45:50 +0530

    node server using http

---
+++ BACKEND_PREPARATION/nodeServer.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const hostname = '127.0.0.1';
+
+const PORT = 3000;
+
+const server = http.createServer((req, res)=>{
+    if (req.url == '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/plain')
+        res.end("home page")
+    }
+    else if (req.url == '/ice-tea') {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/plain')
+        res.end("ice tea is there")
+    }
+    else{ {
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'text/plain')
+            res.end("Kaha aa gai?")
+        }
+    }
+})
+
+server.listen(PORT, hostname, () => {
+    console.log(`Server is Running on http://${hostname}:${PORT}`);
+});
